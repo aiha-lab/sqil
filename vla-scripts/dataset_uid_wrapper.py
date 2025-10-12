37b10ca316c47ae7db89eb9000e31f9bb90802a7
Seong-min-Park <58762739+Seong-min-Park@users.noreply.github.com>
2025-10-13 02:11:40 +0900

    Delete vla-scripts/dataset_uid_wrapper.py

--- vla-scripts/dataset_uid_wrapper.py
+++
@@ -1,9 +0,0 @@
-from torch.utils.data import IterableDataset
-
-class IndexedIterableDataset(IterableDataset):
-    def __init__(self, base_ds):
-        self.base = base_ds  # RLDSDataset (Iterable)
-    def __iter__(self):
-        for idx, item in enumerate(self.base):
-            item["uid"] = item.get("uid", str(idx))
-            yield item
